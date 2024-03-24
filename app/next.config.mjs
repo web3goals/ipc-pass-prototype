a8e99cda3ee2d0a27d99324304fa1c16942d9fc4
kiv1n <aloha@kiv1n.ru>
2024-03-24 16:09:01 +0300

    Update next config

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
+  webpack: (config, { webpack }) => {
     config.resolve.fallback = {
       "cpu-features": false,
     };
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /(cpu-features|sshcrypto\.node)/u,
+      })
+    );
     return config;
   },
 };
